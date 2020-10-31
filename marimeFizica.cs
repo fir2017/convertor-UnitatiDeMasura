93d6938f1f6ea16f6a683daf2de3ba0a15d6928d
fir2017 <razvantrufin99x@gmail.com>
2020-11-01 00:01:47 +0200

    Create marimeFizica.cs

---
+++ marimeFizica.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace convertorUnitatiDeMasura
+{
+    public class marimeFizica
+    {
+        public string marimeaFizicaMasurata;
+        public string indicativMarimeFizica;
+        public string explicatie;
+        public string simbol;
+        public string informatii;
+    }
+}
